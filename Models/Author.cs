d3d0f200cdaab487d5f7294dcf6aba428299dd1d
Chad Thomas <chad.c.thomas@gmail.com>
2020-02-25 07:40:01 -0500

    add domain model

---
+++ Models/Author.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Fisher.Bookstore.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
