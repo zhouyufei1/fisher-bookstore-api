d74a8cffe72c2a691e5efdc252bd90fbf7a8d09e
Chad Thomas <chad.c.thomas@gmail.com>
2020-02-25 18:44:42 -0500

    implement method signature for repos

--- Services/IBooksRepository.cs
+++ Services/IBooksRepository.cs
@@ -1,8 +1,14 @@
+using System.Collections.Generic;
+using Fisher.Bookstore.Models;
+
 namespace Fisher.Bookstore.Services
 {
-
     public interface IBooksRepository
     {
-
+        IEnumerable<Author> GetBooks();
+        Author GetBook(int booksId);
+        void AddBook(Book book);
+        void UpdateBook(Book book);
+        void DeleteBook(Book book);
     }
 }
