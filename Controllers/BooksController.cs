35a47970af2d88018b1fc7a4f90f0daf82928da3
zhouyufei1 <zhou.2153@osu.edu>
2020-03-24 19:12:32 -0400

    a

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,8 +1,58 @@
-namespace Fisher.Bookstore.Controllers
+using Microsoft.AspNetCore.Mvc;
+using Fisher.Bookstore.Models;
+using Fisher.Bookstore.Services;
+
+namespace Fisher.Bookstore.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private IBooksRepository booksRepository;
 
-    public class BooksController
-    {
+        public BooksController(IBooksRepository repository)
+        {
+            booksRepository = repository;
+        }
+    [HttpGet("{bookId")]
 
-    }
+    public IActionResult Get(int bookId)
+    {
+        if(!booksRepository.BookExists(bookId))
+        {
+            return NotFound();
+        }
+        return Ok(booksRepository.GetBooks());
+    }
+    [HttpPost]
+    public IActionResult Post([FromBody]Book book)
+    {
+        var bookId = booksRepository.AddBook(book);
+        return Created($"https://localhost:5001/api/books/{bookId}", book);
+    }
+    [HttpPut("{bookId}")]
+    public IActionResult Put(int bookId, [FromBody] Book book)
+    {
+        if(bookId != book.Id)
+        {
+            return BadRequest();
+        }
+        if(!booksRepository.BookExists(bookId))
+        {
+            return NotFound();
+        }
+        booksRepository.UpdateBook(book);
+        return Ok(book);
+    }
+    [HttpDelete("{bookId}")]
+    public IActionResult Delete(int bookId)
+    {
+        if(!booksRepository.BookExists(bookId))
+        {
+            return NotFound();
+        }
+        booksRepository.DeleteBook(bookId);
+        return Ok();
+    }
+    }
 }
