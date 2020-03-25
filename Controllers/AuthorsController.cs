90a7a01ec8ecd2b65e262fcc0ceb5884225f8891
zhouyufei1 <zhou.2153@osu.edu>
2020-03-25 03:06:45 -0400

    a

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,8 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Fisher.Bookstore.Models;
+using Fisher.Bookstore.Services;
+
 namespace Fisher.Bookstore.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthorsController : ControllerBase
+    {        private IAuthorsRepository AuthorsRepository;
+        public AuthorsController(IAuthorsRepository repository)
+        {
+        AuthorsRepository = repository;
+        }
 
-    public class AuthorsController
-    {
+       
+    [HttpGet]
 
-    }
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+    [HttpPost]
+    public IActionResult Post([FromBody]Author author)
+    {
+        var bookId = AuthorsRepository.AddAuthor(author);
+        return Created($"https://localhost:5001/api/authors/{author}", author);
+    }
+    [HttpPut("{authorId}")]
+    public IActionResult Put(int authorId, [FromBody] Author author)
+    {
+        if(authorId != author.Id)
+        {
+            return BadRequest();
+        }
+        if(!AuthorsRepository.AuthorExists(authorId))
+        {
+            return NotFound();
+        }
+       AuthorsRepository.UpdateAuthor(author);
+        return Ok(author);
+    }
+    [HttpDelete("{authorId}")]
+    public IActionResult Delete(int authorId)
+    {
+        if(!AuthorsRepository.AuthorExists(authorId))
+        {
+            return NotFound();
+        }
+        AuthorsRepository.DeleteAuthor(authorId);
+        return Ok();
+    }
+    }
 }
--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace Fisher.Bookstore
             services.AddControllers();
             services.AddCors();
             services.AddSingleton<IBooksRepository, TestBooksRepository>();
+            services.AddSingleton<IAuthorsRepository, TestAuthorsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
